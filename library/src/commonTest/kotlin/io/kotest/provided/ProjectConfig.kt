5b7c009edcbda144189dfd6d739828d45900c18f
Mauro Molin <molin.mauro@gmail.com>
2026-06-19 19:49:19 +0100

    Make tests concurrent

--- library/src/commonTest/kotlin/io/kotest/provided/ProjectConfig.kt
+++ library/src/commonTest/kotlin/io/kotest/provided/ProjectConfig.kt
@@ -2,7 +2,9 @@ package io.kotest.provided
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.names.DuplicateTestNameMode
+import io.kotest.engine.concurrency.SpecExecutionMode
 
 class ProjectConfig: AbstractProjectConfig() {
     override val duplicateTestNameMode = DuplicateTestNameMode.Error
+    override val specExecutionMode = SpecExecutionMode.Concurrent
 }
