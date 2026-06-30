a6083680f95f233ee967a860fef5d4f1dc7a24a1
Mauro Molin <molin.mauro@gmail.com>
2026-06-30 22:41:34 +0100

    Add RelativeInstantLocalizer

---
+++ library/src/commonMain/kotlin/dev/mmauro/datetimepolyglot/localizers/relative/RelativeInstantLocalizer.kt
@@ -0,0 +1,89 @@
+package dev.mmauro.datetimepolyglot.localizers.relative
+
+import dev.mmauro.datetimepolyglot.PlatformLocale
+import dev.mmauro.datetimepolyglot.TickingValue
+import dev.mmauro.datetimepolyglot.Zoned
+import dev.mmauro.datetimepolyglot.getDefaultLocale
+import dev.mmauro.datetimepolyglot.localizers.PolyglotReferenceValueLocalizer
+import dev.mmauro.datetimepolyglot.localizers.localize
+import dev.mmauro.datetimepolyglot.localizers.localizeAsFlow
+import kotlinx.coroutines.flow.Flow
+import kotlin.time.Clock
+import kotlin.time.Instant
+
+typealias RelativeInstantOptions = RelativeDurationOptions
+
+/**
+ * Localizes an [Instant] relative to another one.
+ *
+ * This class is a simple convenience over [RelativeDurationLocalizer], simply calculating the diff and forwarding the localization request
+ * to it. As this implements [PolyglotReferenceValueLocalizer], this allows to use [localizeAsFlow].
+ *
+ * For this reason, the [options] required here are identical to [RelativeDurationLocalizer].
+ *
+ * Examples:
+ * - `10 minutes ago`
+ * - `in 1 hour`
+ * - `4h ago`
+ * - `4 hr. ago`
+ * - `in 2 days`
+ *
+ * @see PolyglotReferenceValueLocalizer
+ */
+class RelativeInstantLocalizer(
+    options: RelativeInstantOptions = RelativeInstantOptions(),
+    locale: PlatformLocale = getDefaultLocale(),
+) : PolyglotReferenceValueLocalizer<Instant> {
+
+    private val relativeDurationLocalizer = RelativeDurationLocalizer(options, locale)
+
+    override fun localize(value: Instant, reference: Zoned<Instant>): TickingValue<String> {
+        return localize(value, reference.value)
+    }
+
+    // As TZ info is not strictly necessary for this, let's also expose a simpler function with a simple Instant as a reference
+    fun localize(value: Instant, reference: Instant): TickingValue<String> {
+        return relativeDurationLocalizer.localize(value - reference)
+    }
+}
+
+/**
+ * Localizes this [Instant] relatively with respect to [reference], with a single unit with the given [options] in the given [locale].
+ *
+ * @see RelativeDurationLocalizer
+ */
+fun Instant.localizeRelative(
+    reference: Instant,
+    options: RelativeInstantOptions = RelativeInstantOptions(),
+    locale: PlatformLocale = getDefaultLocale(),
+): TickingValue<String> {
+    return RelativeInstantLocalizer(options, locale).localize(this, reference)
+}
+
+/**
+ * Localizes this [Instant] relatively with respect to [clock], with a single unit with the given [options] in the given [locale].
+ *
+ * @see RelativeDurationLocalizer
+ */
+fun Instant.localizeRelative(
+    options: RelativeInstantOptions = RelativeInstantOptions(),
+    locale: PlatformLocale = getDefaultLocale(),
+    clock: Clock = Clock.System,
+): TickingValue<String> {
+    return RelativeInstantLocalizer(options, locale).localize(this, clock)
+}
+
+/**
+ * Localizes this [Instant] relatively with respect to [clock], with a single unit with the given [options] in the given [locale], returning
+ * a [Flow].
+ *
+ * @see RelativeDurationLocalizer
+ * @see localizeAsFlow
+ */
+fun Instant.localizeRelativeAsFlow(
+    options: RelativeInstantOptions = RelativeInstantOptions(),
+    locale: PlatformLocale = getDefaultLocale(),
+    clock: Clock = Clock.System,
+): Flow<String> {
+    return RelativeInstantLocalizer(options, locale).localizeAsFlow(this, clock)
+}
---
+++ library/src/commonTest/kotlin/dev/mmauro/datetimepolyglot/localizers/relative/RelativeInstantLocalizerTest.kt
@@ -0,0 +1,37 @@
+package dev.mmauro.datetimepolyglot.localizers.relative
+
+import dev.mmauro.datetimepolyglot.LOCALE_ENGLISH
+import dev.mmauro.datetimepolyglot.TickingValue
+import dev.mmauro.datetimepolyglot.shouldBeLocalizedAs
+import io.kotest.core.spec.style.FunSpec
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Instant
+
+class RelativeInstantLocalizerTest : FunSpec({
+    // There's not much to test here, as all the logic is forwarded to RelativeDurationLocalizer
+    context("smoke tests") {
+        val localizer = RelativeInstantLocalizer(locale = LOCALE_ENGLISH)
+        val instantA = Instant.parse("2026-04-05T10:00:00.000Z")
+        val instantB = Instant.parse("2026-04-05T11:15:00.000Z")
+        test("past") {
+            instantA.localizeRelative(reference = instantB) shouldBeLocalizedAs TickingValue(
+                value = "1 hour ago",
+                nextTick = 45.minutes,
+            )
+        }
+        test("now") {
+            instantA.localizeRelative(reference = instantA) shouldBeLocalizedAs TickingValue(
+                value = "now",
+                nextTick = 1.seconds,
+            )
+        }
+        test("future") {
+            instantB.localizeRelative(reference = instantA) shouldBeLocalizedAs TickingValue(
+                value = "in 1 hour",
+                nextTick = 15.minutes + 1.nanoseconds,
+            )
+        }
+    }
+})
