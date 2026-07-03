eb52115f4cc850952fb33ea1dbd8ff287b30338a
Mauro Molin <molin.mauro@gmail.com>
2026-07-03 19:50:43 +0100

    Fix RelativeInstantLocalizerTest

--- library/src/commonTest/kotlin/dev/mmauro/datetimepolyglot/localizers/relative/RelativeInstantLocalizerTest.kt
+++ library/src/commonTest/kotlin/dev/mmauro/datetimepolyglot/localizers/relative/RelativeInstantLocalizerTest.kt
@@ -12,23 +12,22 @@ import kotlin.time.Instant
 class RelativeInstantLocalizerTest : FunSpec({
     // There's not much to test here, as all the logic is forwarded to RelativeDurationLocalizer
     context("smoke tests") {
-        val localizer = RelativeInstantLocalizer(locale = LOCALE_ENGLISH)
         val instantA = Instant.parse("2026-04-05T10:00:00.000Z")
         val instantB = Instant.parse("2026-04-05T11:15:00.000Z")
         test("past") {
-            instantA.localizeRelative(reference = instantB) shouldBeLocalizedAs TickingValue(
+            instantA.localizeRelative(reference = instantB, locale = LOCALE_ENGLISH) shouldBeLocalizedAs TickingValue(
                 value = "1 hour ago",
                 nextTick = 45.minutes,
             )
         }
         test("now") {
-            instantA.localizeRelative(reference = instantA) shouldBeLocalizedAs TickingValue(
+            instantA.localizeRelative(reference = instantA, locale = LOCALE_ENGLISH) shouldBeLocalizedAs TickingValue(
                 value = "now",
                 nextTick = 1.seconds,
             )
         }
         test("future") {
-            instantB.localizeRelative(reference = instantA) shouldBeLocalizedAs TickingValue(
+            instantB.localizeRelative(reference = instantA, locale = LOCALE_ENGLISH) shouldBeLocalizedAs TickingValue(
                 value = "in 1 hour",
                 nextTick = 15.minutes + 1.nanoseconds,
             )
