e931048fdcdf28495d3bd0d542e91bedf8c2360b
Mauro Molin <molin.mauro@gmail.com>
2026-07-05 12:54:29 +0100

    Fix build failure

--- buildSrc/src/main/java/dev/mmauro/datetimepolyglot/buildlogic/tasks/generateCldrCode/GenerateCldrCodeTask.kt
+++ buildSrc/src/main/java/dev/mmauro/datetimepolyglot/buildlogic/tasks/generateCldrCode/GenerateCldrCodeTask.kt
@@ -3,31 +3,40 @@ package dev.mmauro.datetimepolyglot.buildlogic.tasks.generateCldrCode
 import dev.mmauro.datetimepolyglot.buildlogic.tasks.generateCldrCode.files.dateTimeJoinerFile
 import dev.mmauro.datetimepolyglot.buildlogic.tasks.generateCldrCode.files.relativeDateOfWeekFiles
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.Directory
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 open class GenerateCldrCodeTask : DefaultTask() {
-    private val output = project.layout.buildDirectory.dir("generated/cldr").get().asFile
+    @OutputDirectory
+    val androidMainOutput = sourceSetDirectory("androidMain")
 
     @OutputDirectory
-    val androidMainOutput = output.resolve("androidMain")
-    @OutputDirectory
-    val webMainOutput = output.resolve("webMain")
+    val webMainOutput = sourceSetDirectory("webMain")
 
     init {
         group = "datetimepolyglot"
         description = "Generates code that has hardcoded CLDR data. Needed for stuff that is not exposed by all localization backends."
-        outputs.dir(output)
+        outputs.dir(androidMainOutput)
+        outputs.dir(webMainOutput)
     }
 
     @TaskAction
     fun run() {
-        output.deleteRecursively()
-        output.mkdirs()
+        outputs.files.forEach {
+            it.deleteRecursively()
+            it.mkdirs()
+        }
 
         dateTimeJoinerFile().writeTo(androidMainOutput)
         dateTimeJoinerFile().writeTo(webMainOutput)
 
         relativeDateOfWeekFiles().writeTo(webMainOutput)
+    }
+
+    private fun sourceSetDirectory(name: String): File {
+        return project.layout.buildDirectory.dir("generated/cldr/$name").get().asFile
     }
 }
