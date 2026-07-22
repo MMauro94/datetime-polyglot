5de5990686ce79e09ee664f006820080aa16ae6b
Mauro Molin <molin.mauro@gmail.com>
2026-07-22 01:07:10 +0100

    Fix typo in PolyglotDateTimeLocalizer doc

--- library/src/commonMain/kotlin/dev/mmauro/datetimepolyglot/localizers/PolyglotDateTimeLocalizer.kt
+++ library/src/commonMain/kotlin/dev/mmauro/datetimepolyglot/localizers/PolyglotDateTimeLocalizer.kt
@@ -3,7 +3,7 @@ package dev.mmauro.datetimepolyglot.localizers
 /**
  * Specialization of [PolyglotValueLocalizer] that allows to localize a date/time value [T] and returns a simple [String].
  *
- * A date/time value is anything component or set of components that may appear in a full date time localization (e.g. year, minute of hour,
+ * A date/time value is any component or set of components that may appear in a full date time localization (e.g. year, minute of hour,
  * time zone, day of the week, etc.)
  *
  * Note that any class implementing this interface is **not** strictly required to return the same exact string for the same value in
